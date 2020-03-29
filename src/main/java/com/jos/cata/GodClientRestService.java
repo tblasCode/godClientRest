ae373e69893470dbce78152d2bdeccc1bbb537f8
tomas.deblas <tomas.deblas@vass.es>
2020-03-29 11:23:01 +0200

    add code: Filter greek gods by n

--- src/main/java/com/jos/cata/GodClientRestService.java
+++ src/main/java/com/jos/cata/GodClientRestService.java
@@ -1,11 +1,12 @@
 package com.jos.cata;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class GodClientRestService {
 
 	public static List<String> filterGods(List<String> gods) {
 		
-		return null;
+		return gods.stream().filter(data -> data.charAt(0) == 'n').collect(Collectors.toList());
 	}
 }
